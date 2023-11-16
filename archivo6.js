c0eb8e81e3fb0ce2bb49e08f46fcb67d729777fe
tamara diaz <tamydiaz.itf@gmail.com>
2023-11-16 15:27:56 -0300

    modificando archivo6

--- archivo6.js
+++ archivo6.js
@@ -2,6 +2,8 @@ function destino(suerte) {
     switch(suerte) {
         case "si":
         console.log("tenees suerte"); break ;
+        case no:
+        console.log("no tenes suerte");break;
         default:
         console.log("tenes mala suerte")
     }
